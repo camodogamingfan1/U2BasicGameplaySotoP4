9198ff61f9a233a7212c9f23519f0febb8b33979
CamodogamingFan1 <751987@student.dusd.net>
2025-04-04 11:44:51 -0700

    issues
    i had issues with my wine prefab and tried to fix it, turns out i'm not that good at it!

--- U2BasicGameplaySotoP4/Assets/scripts/SpawnManager.cs
+++ U2BasicGameplaySotoP4/Assets/scripts/SpawnManager.cs
@@ -7,20 +7,50 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    public GameObject winePrefab;
+    private float spawnPosX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {   // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length); 
-            Instantiate(animalPrefabs[0], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
-        }
+
     }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        GameObject prefabToSpawn = animalPrefabs[animalIndex];
+
+        // Adjust Y position based on prefab's original Y position
+        float spawnHeight = prefabToSpawn.GetComponent<Collider>().bounds.extents.y;
+
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnHeight, spawnPosZ);
+        Instantiate(prefabToSpawn, spawnPos, prefabToSpawn.transform.rotation);
+    }
+    void SpawnWine(string name)
+    {
+        // Instantiate the wine prefab at position (0, 2, 0)
+        GameObject wine = Instantiate(winePrefab, new Vector3(0, 1, 0), Quaternion.identity);
+
+        // Adjust the spawn position based on the object's size (using the collider's bounds)
+        Collider wineCollider = wine.GetComponent<Collider>();
+        if (wineCollider != null)
+        {
+            // Adjust the spawn height so it appears above the ground, taking the collider's height into account
+            float heightOffset = wineCollider.bounds.extents.y;
+            wine.transform.position = new Vector3(wine.transform.position.x, 2 + heightOffset, wine.transform.position.z);
+        }
+
+        // Optionally set the name if needed
+        wine.name = name;
+    }
+
+
+
 }
